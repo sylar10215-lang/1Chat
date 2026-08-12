158f8ba142a2aeb799cf2147b591dbc01b7a283c
sylar10215-lang <sylar10215@gmail.com>
2026-08-12 08:36:08 +0700

    firebase-messaging-sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDbMaGhHmPTReldMPckDk0G_Vk2gqiRjs",
+  projectId: "chat01-bd6fd",
+  messagingSenderId: "120948633141",
+  appId: "1:120948633141:web:36ae2459dda98164094b7c"
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  const notificationTitle = payload.notification.title || 'ข้อความใหม่จาก MJ Chat';
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
